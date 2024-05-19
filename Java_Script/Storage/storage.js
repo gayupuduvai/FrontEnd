5546d4879826fb66b1fa1217bd91240911db829b
gayupuduvai <gayupuduvai@gmail.com>
2024-05-19 23:10:09 +0530

    19May

--- Java_Script/Storage/storage.js
+++ Java_Script/Storage/storage.js
@@ -1,22 +1,40 @@
-function localstorage() {
+function formlocalStorage() {
   let Username = document.getElementById("username").value;
   let Password = document.getElementById("pswd").value;
 
   let nameError = document.getElementById("nameError");
   let pswdError = document.getElementById("pswdError");
 
-  var validName = "Gayathri",
-    password = "Gayu@2024";
-  if (Username == validName && Password == password) {
-    localstorage.setItem(Username);
-    var getLocalItem = localstorage.getItem("Username");
-    console.log(getLocalItem);
-    localstorage.setItem(Password);
-    window.location.assign(
-      "http://127.0.0.1:5500/Java_Script/Storage/hompage.html"
-    );
+  let validName = "Gayathri";
+  let validPassword = "Gayu@2024";
+  
+  nameError.textContent = "";
+  pswdError.textContent = "";
+
+  if (Username === validName && Password === validPassword) {
+    
+    localStorage.setItem("name", Username);
+    // var data = localStorage.getItem("name");
+    // console.log(data);
+    window.location.assign("http://127.0.0.1:5500/Java_Script/Storage/hompage.html") 
   } else {
-    nameError.textContent = "Name Required";
-    pswdError.textContent = "Password required";
+    if (Username === ""){
+      nameError.textContent = "Name is Required";
+    } else if (Username !== validName){
+      nameError.textContent = "Invalid Name";
+    }
+
+    if (Password === "") {
+      pswdError.textContent = "Password is required";
+    } else if (Password !== validPassword) {
+      pswdError.textContent = "Invalid Password";
+    }
   }
+  
 }
+
+
+document.getElementById("loginPage").addEventListener("submit", function(event) {
+  event.preventDefault();
+  formlocalStorage();
+});
