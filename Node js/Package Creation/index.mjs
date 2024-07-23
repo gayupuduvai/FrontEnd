23d137bfdee029d106b2b140db42ad8a40a6e58e
sandhiya121298 <sandhiyaraj1212@gmail.com>
2024-07-23 16:11:19 +0530

    22nd july

--- Node js/Package Creation/index.mjs
+++ Node js/Package Creation/index.mjs
@@ -20,5 +20,50 @@
 //   rl.close();
 // });
 
-import {input} from '@inquirer/prompts';
-const answer = await input ({message: "Enter your name"});
+// import {input} from '@inquirer/prompts';
+// const answer = await input ({message: "Enter your name"});
+
+import inquirer from 'inquirer';
+
+const questions = [
+  {
+    type: 'input',
+    name: 'firstName',
+    message: "What's your first name?"
+  },
+  {
+    type: 'input',
+    name: 'lastName',
+    message: "What's your last name?"
+  },
+  {
+    type: 'input',
+    name: 'phoneNumber',
+    message: 'What\'s your phone number?',
+    validate: function(value) {
+      const pass = value.match(/^\d{10}$/);
+      if (pass) {
+        return true;
+      }
+      return 'Please enter a valid 10-digit phone number.';
+    }
+  }
+];
+
+inquirer.prompt(questions)
+  .then(answers => {
+    console.log('User entered:');
+    console.log('First Name:', answers.firstName);
+    console.log('Last Name:', answers.lastName);
+    console.log('Phone Number:', answers.phoneNumber);
+  });
+
+
+
+
+
+
+
+
+
+
