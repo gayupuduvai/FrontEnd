b8a582a0d164dab4b9563284b8698a92a6b0fc63
sandhiya121298 <sandhiyaraj1212@gmail.com>
2024-08-14 17:41:42 +0530

    14August

--- Mongodb_Express/src/controllers/student-controller.js
+++ Mongodb_Express/src/controllers/student-controller.js
@@ -1,4 +1,6 @@
 const { StudentModel } = require("../models/student-model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const addStudent = async (req, res) => {
   try {
@@ -20,7 +22,7 @@ const getStudent = async (req, res) => {
 
     res.status(201).json({
       status: "success",
-      message: "Student added successfully",
+      message: "Retrieved student successfully",
       data: students,
     });
   } catch (error) {
@@ -28,4 +30,121 @@ const getStudent = async (req, res) => {
   }
 };
 
-module.exports = { addStudent, getStudent };
+const getStudentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const student = await StudentModel.findById(id);
+
+    res.status(201).json({
+      status: "success",
+      message: "Retrieved student successfully",
+      data: student,
+    });
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
+
+const getStudentByIdAndUpdate = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const student = await StudentModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: "Student updated successfully",
+      data: student,
+    });
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
+
+const deleteStudentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await StudentModel.findByIdAndDelete(id);
+
+    res.status(201).json({
+      status: "success",
+      message: "Student deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { userName, password } = req.body;
+
+  const student = await StudentModel.findOne({ userName });
+
+  try {
+    if (!student) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, student.password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ status: "error", message: "Invalid credentials" });
+    }
+
+    // Generate a token
+    const token = jwt.sign({ id: student.id }, "ABC");
+
+    res.status(201).json({
+      status: "success",
+      message: "Student logged in successfully",
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
+
+const register = async (req, res) => {
+  const { userName, password } = req.body;
+
+  const student = await StudentModel.exists({ userName });
+
+  if (student) {
+    return res
+      .status(409)
+      .json({ status: "error", message: "Username already exists" });
+  }
+
+  // Generate a salt
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  // Hash the password with the salt
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newStudent = await StudentModel({ userName, password: hashedPassword });
+
+  await newStudent.save();
+
+  res
+    .status(201)
+    .json({ status: "success", message: "Student registered successfully" });
+};
+
+module.exports = {
+  addStudent,
+  getStudent,
+  getStudentById,
+  getStudentByIdAndUpdate,
+  deleteStudentById,
+  login,
+  register,
+};
--- Mongodb_Express/src/routes/student-route.js
+++ Mongodb_Express/src/routes/student-route.js
@@ -1,9 +1,40 @@
 const express = require("express");
-const { addStudent, getStudent } = require("../controllers/student-controller");
+const {
+  addStudent,
+  getStudent,
+  getStudentById,
+  getStudentByIdAndUpdate,
+  deleteStudentById,
+  login,
+  register,
+} = require("../controllers/student-controller");
+const { authMiddlware } = require("../middleware/auth-middleware");
+const { idCheckerMiddleware } = require("../middleware/idChecker-middleware");
+
 const router = express.Router();
+
+router.post("/login", login);
+
+router.post("/register", register);
 
 router.post("/add", addStudent);
 
 router.get("/get", getStudent);
+
+router.get("/get/:id", idCheckerMiddleware, authMiddlware, getStudentById);
+
+router.patch(
+  "/update/:id",
+  idCheckerMiddleware,
+  authMiddlware,
+  getStudentByIdAndUpdate
+);
+
+router.delete(
+  "/delete/:id",
+  idCheckerMiddleware,
+  authMiddlware,
+  deleteStudentById
+);
 
 module.exports = router;
